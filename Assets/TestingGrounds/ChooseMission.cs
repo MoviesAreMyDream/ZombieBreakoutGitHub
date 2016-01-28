ee16dac75e60f2dba325bd15538560ea683bcc8a
ObamaPls <nyonatanb@yahoo.com>
2016-01-28 18:04:13 +0800

    Added mission choosing script
    But it's not yet functional D:

---
+++ Assets/TestingGrounds/ChooseMission.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChooseMission : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnTriggerEnter(Collider something)
+	{
+		if(something.tag ==  "Player")
+		{
+
+		}
+	}
+}
