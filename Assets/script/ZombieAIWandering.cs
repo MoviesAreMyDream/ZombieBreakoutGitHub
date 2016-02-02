648ce163c066370e43a6daf41e5c001a402ec0c2
Rasyid17 <rex.oxide@gmail.com>
2016-02-02 17:55:53 +0800

    ZombieAI Wandering

---
+++ Assets/script/ZombieAIWandering.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieAIWandering : MonoBehaviour {
+
+	public float wanderRadius;
+	public float wanderTime;
+	
+	private Transform target;
+	private NavMeshAgent agent;
+	private float Timer;
+	public Animator ZombiePref;
+
+	// Use this for initialization
+	void Start () {
+	
+		agent = GetComponent<NavMeshAgent>();
+		Timer = wanderTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Timer += Time.deltaTime;
+
+		if (Timer >= wanderTime) {
+			Vector3 newPos = RandomNavSphere (transform.position, wanderRadius, -1);
+			agent.SetDestination (newPos);
+
+			if (agent.SetDestination (newPos)) {
+				ZombiePref.SetBool ("PlayerIsDead", true);
+				ZombiePref.SetBool ("PlayerInRange", true);
+			}
+
+			print("Zombie Stopped");
+
+			StartCoroutine(SetTimerZero(3f));
+
+
+
+		} else {
+
+			ZombiePref.SetBool ("PlayerIsDead", false);
+			ZombiePref.SetBool ("PlayerInRange", false);
+		}
+	}
+
+	public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+	{
+		Vector3 randDirection = Random.insideUnitSphere*dist;
+		randDirection += origin;
+		NavMeshHit navHit;
+		NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+		return navHit.position;
+	}
+		
+	IEnumerator SetTimerZero(float IdleDelay)
+	{
+		yield return new WaitForSeconds (IdleDelay);
+
+		Timer = 0;
+
+	}
+
+}
