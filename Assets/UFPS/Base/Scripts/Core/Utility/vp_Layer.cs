46c66e8b119959f925860c9373b12484ea5b05ae
Rasyid17 <rex.oxide@gmail.com>
2016-02-22 17:47:04 +0800

    set gui back follow player head
    remove some un-use scripts

--- Assets/UFPS/Base/Scripts/Core/Utility/vp_Layer.cs
+++ Assets/UFPS/Base/Scripts/Core/Utility/vp_Layer.cs
@@ -38,7 +38,7 @@ public sealed class vp_Layer
 	public const int MovingPlatform = 28;	// used to make the player snap to, rotate with and inherit speed from moving platforms such as elevators
 	public const int Debris = 29;			// used to filter out small flying rubble on collision 
 	public const int LocalPlayer = 30;		// used for weaponcamera rendering, and to distinguish the local player from multiplayer remote ones
-	public const int Weapon = 5;			// used for weaponcamera rendering
+	public const int Weapon = 31;			// used for weaponcamera rendering
 
 	public static class Mask
 	{
