4088270d36353c7da2ca148ddcae9c8fa8229b56
acemaster93 <m.syazwan.haikal@gmail.com>
2016-03-23 09:55:21 +0800

    update tutorial 22 march

---
+++ Assets/script/Tutorial.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tutorial : MonoBehaviour {
+
+
+    public GameObject AllControl;
+    public GameObject Fire;
+    public GameObject ChangeWeaponRight;
+    public GameObject ChangeWeaponLeft;
+    public GameObject Sprint;
+    public GameObject Use;
+    public GameObject ClosePortal;
+    public GameObject Look;
+    public GameObject Pause;
+    public GameObject Move;
+
+    public bool complete;
+        
+    void Start () {
+        AllControl.SetActive(true);
+        Fire.SetActive(false);
+        ChangeWeaponRight.SetActive(false);
+        ChangeWeaponLeft.SetActive(false);
+        Sprint.SetActive(false);
+        Use.SetActive(false);
+        ClosePortal.SetActive(false);
+        Look.SetActive(false);
+        Pause.SetActive(false);
+        Move.SetActive(false);
+
+        complete = false;
+	}
+	
+
+	void Update () {
+	   // if(Input.GetKeyDown)
+	}
+}
