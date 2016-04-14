bde12f919cbb034d7911049a10de4ee9138aa29f
Rasyid17 <rex.oxide@gmail.com>
2016-04-14 16:31:57 +0800

    update 14th april
    - now has cooldown for lasergun but still need to hold it to finish up
    cooldown
    - sword now has no more zooming when switch to it

---
+++ Assets/script/KassimnyerAdjustment/SwordFOV.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SwordFOV : MonoBehaviour {
+
+	private vp_FPPlayerEventHandler Player;
+
+	// Use this for initialization
+	void Start () {
+	
+		Player = GameObject.FindObjectOfType (typeof(vp_FPPlayerEventHandler)) as vp_FPPlayerEventHandler;
+	}
+
+	void Awake(){
+
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if(Player.CurrentWeaponName.Get() == "6Sword")
+		{
+			gameObject.GetComponent<vp_FPCamera>().RenderingFieldOfView = 110;
+		}
+
+	}
+}
+
+
