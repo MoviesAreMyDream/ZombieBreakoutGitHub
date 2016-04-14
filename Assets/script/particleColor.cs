c75425dd29544506f9bcbd5e0146f18a31a13c49
Farhana Kamaruddin <shiho.gendou@gmail.com>
2016-04-14 17:36:28 +0800

    Fix dust particles not matching night lighting
    Add new script "particleColor" to change the material color of dust
    particles when the environment change into night cycle.

---
+++ Assets/script/particleColor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class particleColor : MonoBehaviour {
+
+	public GameObject Sun;
+	public GameObject Dust;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		/*if (Sun.GetComponent<DayNightCycle>().TimeFloat >= 13 && Sun.GetComponent<DayNightCycle>().TimeFloat <= 13.5)
+		{
+			Dust.gameObject.GetComponent<Renderer> ().material.SetColor("_TintColor", new Color (0.2f, 0.2f, 0.2f, 0.35f));
+		}*/
+
+		if (Sun.GetComponent<DayNightCycle>().TimeFloat >= 13.75)
+		{
+			Dust.gameObject.GetComponent<Renderer> ().material.SetColor("_TintColor", new Color (0.37f, 0.38f, 0.43f, 0.17f));
+		}
+
+	}
+}
