56cd593a3f3251c9c9f0efb0b14cf1c0fabf66a6
n931009 <n931009@gmail.com>
2016-07-25 17:12:05 +0800

    norris mon

---
+++ Assets/enterTriggerVoice.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enterTriggerVoice : MonoBehaviour {
+
+    public GameObject oldMessage;
+    public GameObject newMessage;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            readyToPlayAudio();
+            //oldMessage.SetActive(false);
+            //newMessage.SetActive(true);
+        }
+    }
+
+    void readyToPlayAudio()
+    {
+        StartCoroutine (playAudio());
+    }
+
+    IEnumerator playAudio()
+    {
+        yield return new WaitForSeconds(0f);
+        oldMessage.SetActive(false);
+        newMessage.SetActive(true);
+        yield return new WaitForSeconds(10f);
+        gameObject.SetActive(false);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //gameObject.SetActive(false);
+        }
+    }
+}
