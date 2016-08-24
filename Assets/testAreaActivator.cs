0eb6796e05ea462db8d92dde1bad0f680b15a484
n931009 <n931009@gmail.com>
2016-08-24 17:21:36 +0800

    norris weds

---
+++ Assets/testAreaActivator.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testAreaActivator : MonoBehaviour {
+
+    public bool canTouch;
+
+	// Use this for initialization
+	void Start () {
+        canTouch = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("t"))
+        {
+            if (canTouch == true)
+            {
+                print("closing portal...");
+            }
+
+            else if (canTouch == false)
+            {
+                print("no portal nearby");
+            }
+            
+        }
+        else
+        {
+            //print("no portal nearby");
+        }
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.tag == "TriggerArea")
+        {
+            canTouch = true;
+            Debug.Log("entered");
+        }
+        else
+        {
+            canTouch = false;
+        }
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+        canTouch = false;
+    }
+}
---
+++ Assets/testAreaTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testAreaTrigger : MonoBehaviour {
+
+    public bool canTrigger;
+
+	// Use this for initialization
+	void Start () {
+        canTrigger = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
